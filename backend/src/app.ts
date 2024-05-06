b4a063e0be9a6703c5a3b0efc7caef6d7b147bb2
Devadavid <s3927112@rmit.edu.vn>
2024-05-06 18:38:44 +0700

    adding cors for backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import postRouter from './routers/postRouter'
-
+import cors from 'cors'
 
 const router = express.Router()
 router.use('/post', postRouter)
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
+
 const app = express()
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(router)
