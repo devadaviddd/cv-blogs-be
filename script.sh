78a0a3f41a2a3bb218e428a59f9af575d43a4067
Devadavid <s3927112@rmit.edu.vn>
2024-05-06 12:41:03 +0700

    test jenkin env var pipeline

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 cp -f  /var/lib/jenkins/workspace/.envvars/.env .
 cat << EOF > /var/lib/jenkins/workspace/cv-blogs-backend-pipeline/backend/.env
-$env.DATABASE_URL
-$DATABASE_URL
+DATABASE_URL=$DATABASE_URL
 EOF
