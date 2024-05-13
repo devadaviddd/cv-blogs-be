ab717e895111c84c931fd947b90069ea25be0ff7
devadaviddd <s3927112@rmit.edu.vn>
2024-05-13 17:31:27 +0700

    test migration

---
+++ prisma/migrations/20240513102258_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `posts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `image` VARCHAR(255) NOT NULL,
+    `title` TEXT NOT NULL,
+    `description` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+INSERT INTO posts (image, title, description)
+VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
+INSERT INTO posts (image, title, description)
+VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
+INSERT INTO posts (image, title, description)
+VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
+INSERT INTO posts (image, title, description)
+VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
+INSERT INTO posts (image, title, description)
+VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
---
+++ prisma/migrations/20240513102745_add_user_table/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `posts` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `posts`;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `image` VARCHAR(255) NOT NULL,
+    `title` TEXT NOT NULL,
+    `description` TEXT NOT NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `uid` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `displayName` VARCHAR(255) NOT NULL,
+    `photoURL` TEXT NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`uid`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`uid`) ON DELETE RESTRICT ON UPDATE CASCADE;
