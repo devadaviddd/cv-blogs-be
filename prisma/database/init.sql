0e0193f72491645ab0d913fb6efe421a61a4d220
devadaviddd <s3927112@rmit.edu.vn>
2024-05-13 18:47:45 +0700

    test migration

--- prisma/database/init.sql
+++ prisma/database/init.sql
@@ -1,18 +1,18 @@
 USE backend-db;
-CREATE TABLE posts (
+CREATE TABLE Post (
   id INT AUTO_INCREMENT PRIMARY KEY,
   image VARCHAR(255) NOT NULL,
   title TEXT(255) NOT NULL,
   description TEXT(255) NOT NULL
 );
 
-INSERT INTO posts (image, title, description)
+INSERT INTO Post (image, title, description)
 VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
-INSERT INTO posts (image, title, description)
+INSERT INTO Post (image, title, description)
 VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
-INSERT INTO posts (image, title, description)
+INSERT INTO Post (image, title, description)
 VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
-INSERT INTO posts (image, title, description)
+INSERT INTO Post (image, title, description)
 VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
-INSERT INTO posts (image, title, description)
+INSERT INTO Post (image, title, description)
 VALUES ('https://img.freepik.com/premium-photo/cardano-blockchain-platform_23-2150411956.jpg', 'Tech Blog', 'This is a blog about technology you should read it more to know more!');
